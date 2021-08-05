26bf36fa6cd371ef81a2e8d26bb1906cf81a13fb
Waverley Leung <waverley.nyc@gmail.com>
2021-08-05 17:22:54 -0400

    fetches data from API and displays notifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/notifications`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((arrayOfNotifications) => {
+        setNotifications(arrayOfNotifications);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Resilia Take Home</header>
+      <div>
+        {notifications &&
+          notifications.map((notificationData) => {
+            return (
+              <div key={notificationData.id}>
+                <h2>{notificationData.title}</h2>
+                <p>{notificationData.details}</p>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
